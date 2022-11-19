17cb8bf7ea1f63069f899c82b1dd6c4a2c79709e
fisvif <shutkovadim@mail.ru>
2022-11-19 22:37:28 +0200

    new animation

--- js/slider.js
+++ js/slider.js
@@ -7,14 +7,48 @@ const swiper = new Swiper('.swiper', {
     }
   });
 
-let heigt = []
+let sliderNext = document.querySelector('.slider-next'),
+    sliderPrew = document.querySelector('.slider-prew')
 
-document.querySelectorAll('.swiper-slide').forEach((e) => {
-	heigt.push(e.offsetHeight)
-	console.log(e.offsetHeight);
-})
-console.log(heigt);
+	sliderNext.onclick = () =>{ 
+		removeClass(document.querySelectorAll('.swiper-slide-prev p')) 
+		removeClass(document.querySelectorAll('.swiper-slide-next p')) 
+		slideTextAnimation()
+	}
+	sliderPrew.onclick = () =>{
+		removeClass(document.querySelectorAll('.swiper-slide-prev p')) 
+		removeClass(document.querySelectorAll('.swiper-slide-next p')) 
+		slideTextAnimation() 
+	}
 
-document.querySelectorAll('.swiper-slide').forEach((e) => {
-	e.style.height = Math.max(heigt) + 'px';
-})
+	slideTextAnimation()
+
+
+function slideTextAnimation()
+{
+  let text = document.querySelectorAll('.swiper-slide-active p')
+
+  text.forEach((e, i) => {
+	if (!(i % 2)) 
+	{
+		e.classList.add('slider-text-left')
+	} else 
+	{
+		e.classList.add('slider-text-right')
+	}
+  })
+	}
+
+function removeClass(elements)
+{
+	
+	elements.forEach((e, i) => {
+		if (!(i % 2)) 
+		{
+			e.classList.remove('slider-text-left')
+		} else 
+		{
+			e.classList.remove('slider-text-right')
+		}
+	})
+}
