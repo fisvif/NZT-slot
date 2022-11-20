62977a543a75f6071969cbe25d8b3aeb34ff2e29
fisvif <shutkovadim@mail.ru>
2022-11-20 19:26:14 +0200

    add new adnimation

---
+++ js/animation.js
@@ -0,0 +1,13 @@
+var i = 0;
+var txt = 'FIND OUT MORE ABOUT GAME'; /* Текст */
+var speed = 85; /* Скорость/длительность эффекта в миллисекундах */
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.querySelector(".text").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+typeWriter()
